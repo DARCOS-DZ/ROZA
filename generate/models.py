3434f838e511d503641fa60c4c0dbd8ac2decadd
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2022-11-24 09:38:24 +0100

    syncronazing id references rosa api

--- generate/models.py
+++ generate/models.py
@@ -1,10 +1,28 @@
 from django.db import models
-
+import requests
+import json
+BASE_API_ROSA="https://rosa.ae/app/v1/api"
 # Create your models here.
 class Flower(models.Model):
     reference=models.CharField(verbose_name="Reference name",unique=True,max_length=200)
+    id_reference = models.IntegerField(null=True,blank=True)
     category = models.ForeignKey("generate.Category", on_delete=models.CASCADE,null=True)
     object_3d=models.FileField(verbose_name="3D Image",upload_to="flowers_obj",null=True,blank=True)
+    def save(self, *args, **kwargs):
+        if self.id_reference=="" or self.id_reference==None or not Flower.objects.filter(id_reference=self.id_reference).exists():
+            try:
+                rosa_api_flowers = requests.get(f"{BASE_API_ROSA}/get_rose")
+                data =  json.loads (rosa_api_flowers.text)
+                my_reference_name = self.reference.replace(' ','').upper()
+                for flower in  data["data"]:
+                    name = str(flower["name"]).replace(' ','').upper()
+                    id_rosa_api = int(flower["id"])
+                    if name == my_reference_name:
+                        print("name",name,"id",id_rosa_api)
+                        self.id_reference=id_rosa_api
+            except Exception as e :
+                print(e)
+        super(Flower, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return self.reference
 class Category(models.Model):
@@ -20,8 +38,25 @@ class Topic(models.Model):
   
 class Vase(models.Model):
     reference=models.CharField(verbose_name="reference",unique=True,max_length=200)
+    id_reference = models.IntegerField(null=True,blank=True)
     object_3d=models.FileField(verbose_name="3D Image",upload_to="vases_obj",null=True,blank=True)
     max = models.PositiveIntegerField(verbose_name="max flowers",default=0)
+    def save(self, *args, **kwargs):
+        if self.id_reference=="" or self.id_reference==None or not Vase.objects.filter(id_reference=self.id_reference).exists():
+            try:
+                rosa_api_vases = requests.get(f"{BASE_API_ROSA}/get_vase")
+                data =  json.loads (rosa_api_vases.text)
+                my_reference_name = self.reference.replace(' ','').upper()
+                print(data)
+                for vase in  data["data"]:
+                    name = str(vase["name"]).replace(' ','').upper()
+                    id_rosa_api = int(vase["id"])
+                    if name == my_reference_name:
+                        print("name",name,"id",id_rosa_api)
+                        self.id_reference=id_rosa_api
+            except Exception as e :
+                print(e)
+        super(Vase, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return self.reference
 
--- generate/views.py
+++ generate/views.py
@@ -20,52 +20,47 @@ def try_except_get_django_model(Model,id_name):
     except Model.DoesNotExist:
         model=None
     return model
-
 def index(request):
-    #/flower=[{rose_id:id,quantity=q},...]&vase_id=id&topic_id=id
+    #/flower=[{rose_id:id,quantity=q},...]&vase_id=id&{topic_id:id,quantity=q}
     if "flower" and "vase" in request.GET:
         """*** GET user package data of flowers and topic"""
         flowers = request.GET.getlist("flower",None)#[{rose_id:id,quantity=q},...]
         vase_id = request.GET.get("vase",None) # vase_id = id
-        topic_id = request.GET.get("topic",None)# topic_id = id
         array_of_flowers=[]
-        topic = try_except_get_django_model(Topic,topic_id)
         vase = try_except_get_django_model(Vase,vase_id)
-        array_json_flowers_from_user={"topic":1}
-
-        for flower in flowers :
-            flower=json.loads(flower)
-            # get flower object from database
+        if vase:
+            array_json_flowers_from_user={}
+            for flower in flowers :
+                flower=json.loads(flower)
+                # get flower object from database
+                try:
+                    flower_object = Flower.objects.get(id_reference=flower["rose_id"])
+                except Flower.DoesNotExist:
+                    flower_object = None
+                """=== combining all flowers 3d obj path in one array ===="""
+                combining_flowers(flower_object,array_json_flowers_from_user,flower,array_of_flowers)
+            """*** Sorting array for syncronizing index ***"""
+            new_array_sorted = []
+            for i in sorted(array_of_flowers, key=lambda d: d['name']):
+                new_array_sorted.append(i["url"])
             try:
-                flower_object = Flower.objects.get(id=flower["rose_id"])
-            except Flower.DoesNotExist:
-                flower_object = None
-            """=== combining all flowers 3d obj path in one array ===="""
-            combining_flowers(flower_object,array_json_flowers_from_user,flower,array_of_flowers)
-        array_of_flowers.append({'name':"topic",'url':topic.object_3d.url})
-        
-        """*** Sorting array for syncronizing index ***"""
-        new_array_sorted = []
-        for i in sorted(array_of_flowers, key=lambda d: d['name']):
-            new_array_sorted.append(i["url"])
-
-        try:
-            print("array_json_flowers_from_user",array_json_flowers_from_user)
-            best_position_json = Position.objects.filter(refernce_json_flowers=array_json_flowers_from_user).first()
-        except Position.DoesNotExist:
-            best_position_json = None 
-        if best_position_json:
-            print("best_position_json",best_position_json.position_file)
-        else:
-            print("best_position_json is None",)
-
-        context= {
-            "array_of_flowers":json.dumps(new_array_sorted ),
-            "vase_obj": vase,
-            "topic_obj":topic,
-            "best_position_json":best_position_json,
-        }
-        return render(request,"base.html",context)
+                array_json_flowers_from_user_sorted = dict(sorted(array_json_flowers_from_user.items()))
+                print("array_json_flowers_from_user",array_json_flowers_from_user_sorted)
+                best_position_json = Position.objects.filter(refernce_json_flowers=array_json_flowers_from_user_sorted,vase=vase).first()
+            except Position.DoesNotExist:
+                best_position_json = None 
+            if best_position_json:
+                print("best_position_json",best_position_json.position_file)
+            else:
+                print("best_position_json is None",)
+            context= {
+                "array_of_flowers":json.dumps(new_array_sorted ),
+                "vase_obj": vase,
+                "best_position_json":best_position_json,
+            }
+            return render(request,"base.html",context)
+        print("vase or topic is none")
+        return render(request,"base.html")
 
     return render(request,"base.html")
 
